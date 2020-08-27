168d4c709856176710606408362ad915f581baaa
Goldenlion5648 <coboudinot@gmail.com>
2020-08-26 23:28:11 -0400

    part 2 done

--- day15.py
+++ day15.py
@@ -11,13 +11,24 @@ prodB = [start_b]
 count = 0
 prevA = start_a
 prevB = start_b
-for i in range(40000000):
+for i in range(5000000):
     curA = (prevA * factor_a)%div
     curB = (prevB * factor_b)%div
     # prodA.append((prodA[-1] * factor_a)%div)
     # prodB.append((prodB[-1] * factor_b)%div)
-    if bin(curA)[-16:] == bin(curB)[-16:]:
-        count += 1
+    while curA % 4 != 0:
+        prevA=curA
+        curA = (prevA * factor_a)%div
+
+    while curB % 8 != 0:
+        prevB=curB
+        curB = (prevB * factor_b)%div
+    if curA % 4 == 0 and curB % 8 == 0:
+        if bin(curA)[-16:] == bin(curB)[-16:]:
+            count += 1
+            # print(i)
+            # print(curA, curB)
+            # break
     if i % 100000 == 0:
         print(i)
     prevA=curA
@@ -29,3 +40,4 @@ print(count)
 # Generator A starts with 512
 # Generator B starts with 191
 #part 1 done in 14:24
+#part 2 done in 10:11
